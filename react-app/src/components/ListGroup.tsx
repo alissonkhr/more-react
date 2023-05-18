233dabd5a3692c125b412e68c18ded435de20582
alissonkhr <alissonricardo68@gmail.com>
2023-05-18 05:43:16 -0700

    list group showing as bootstrap formatted component

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,13 @@
 function ListGroup() {
-  return <h1>List Group</h1>;
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">1</li>
+      <li className="list-group-item">2</li>
+      <li className="list-group-item">3</li>
+      <li className="list-group-item">4</li>
+      <li className="list-group-item">5</li>
+    </ul>
+  );
 }
 
 export default ListGroup;
