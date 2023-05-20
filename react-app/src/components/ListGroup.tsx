f47d190a0fdf47c31b36a38488d76095454ea435
alissonkhr <alissonricardo68@gmail.com>
2023-05-19 21:19:23 -0700

    list has header and wrapped in fragment syntax

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">1</li>
-      <li className="list-group-item">2</li>
-      <li className="list-group-item">3</li>
-      <li className="list-group-item">4</li>
-      <li className="list-group-item">5</li>
-    </ul>
+    <>
+        <h1>List</h1>
+        <ul className="list-group">
+          <li className="list-group-item">1</li>
+          <li className="list-group-item">2</li>
+          <li className="list-group-item">3</li>
+          <li className="list-group-item">4</li>
+          <li className="list-group-item">5</li>
+        </ul>
+    </>
   );
 }
 
