2149eeedb9636af1b2792ab7694f783c28ed872e
alissonkhr <alissonricardo68@gmail.com>
2023-05-21 14:03:16 -0700

    list group has dynamic items now

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,14 @@
 function ListGroup() {
+  const items = ["Las Vegas", "Narnia", "The Shire", "Hyrule"];
+
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">1</li>
-          <li className="list-group-item">2</li>
-          <li className="list-group-item">3</li>
-          <li className="list-group-item">4</li>
-          <li className="list-group-item">5</li>
-        </ul>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
