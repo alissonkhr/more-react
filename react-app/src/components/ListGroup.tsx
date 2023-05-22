9a83c88684b84f82abd428e3c5849f791a23164e
alissonkhr <alissonricardo68@gmail.com>
2023-05-22 13:09:36 -0700

    changed conditional to go inside of render instead

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,17 +2,10 @@ function ListGroup() {
   let items = ["Las Vegas", "Narnia", "The Shire", "Hyrule"];
   items = [];
 
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
-
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found.</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
