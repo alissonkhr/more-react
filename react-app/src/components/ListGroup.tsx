109cdc488d5b65609dbf448a777f1b4ce71c88cf
alissonkhr <alissonricardo68@gmail.com>
2023-05-23 13:00:15 -0700

    list showing correctly with bootstrap styling

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,15 @@
 function ListGroup() {
   let items = ["Las Vegas", "Narnia", "The Shire", "Hyrule"];
-  items = [];
 
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 ? <p>No item found.</p> : null}
+      {items.length === 0 && <p>No item found.</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
