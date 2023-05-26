7686b3213445271f9ec03377550cfe2db2e59945
alissonkhr <alissonricardo68@gmail.com>
2023-05-25 20:12:20 -0700

    moved click logic to a separate function by using event handler

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,17 +1,19 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Las Vegas", "Narnia", "The Shire", "Hyrule"];
+
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found.</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log("Clicked")}
-          >
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
