103dd7b71acd8c6231f0533680ab6fe97ece3f8d
alissonkhr <alissonricardo68@gmail.com>
2023-05-27 13:06:13 -0700

    adding properties to component for list

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["Las Vegas", "Narnia", "The Shire", "Hyrule"];
+
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
