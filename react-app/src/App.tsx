249243f9bc209565ef11f3759b6f3ba86d8c9be8
alissonkhr <alissonricardo68@gmail.com>
2023-05-29 13:09:23 -0700

    alert component showing in app and with bootstrap styling

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["Las Vegas", "Narnia", "The Shire", "Hyrule"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
-    <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+    <div className="alert alert-primary">
+      <Alert />
     </div>
   );
 }
