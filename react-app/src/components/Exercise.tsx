c86007af002d27255a04ad833542531b23b0884a
alissonkhr <alissonricardo68@gmail.com>
2023-05-31 20:12:20 -0700

    added button component to app

---
+++ react-app/src/components/Exercise.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Exercise = () => {
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">
+        Button
+      </button>
+    </div>
+  );
+};
+
+export default Exercise;
