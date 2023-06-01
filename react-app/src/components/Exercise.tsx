850fe839be40b5d7fadc93a6d6b3bc32477942f9
alissonkhr <alissonricardo68@gmail.com>
2023-06-01 13:42:45 -0700

    console log when button is clicked successful

--- react-app/src/components/Exercise.tsx
+++ react-app/src/components/Exercise.tsx
@@ -3,7 +3,13 @@ import React from "react";
 const Exercise = () => {
   return (
     <div>
-      <button type="button" className="btn btn-primary">
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => {
+          console.log("clicked");
+        }}
+      >
         Button
       </button>
     </div>
