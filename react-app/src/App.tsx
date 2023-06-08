5b1ebfdd1956af6eee7f29f2f83bdda58a1a08eb
alissonkhr <alissonricardo68@gmail.com>
2023-06-08 12:46:53 -0700

    header shows in app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,7 @@
 import Alert from "./components/Alert";
 import Exercise from "./components/Exercise";
 import ListGroup from "./components/ListGroup";
+import Header from "./components/Header";
 
 function App() {
   let items = ["Narnia", "Las Vegas", "Land of Ooo"];
@@ -9,31 +10,34 @@ function App() {
   };
 
   return (
-    <div className="alert alert-primary">
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-      <Exercise
-        color="danger"
-        onClick={() => {
-          console.log("first button");
-        }}
-      >
-        Click me!
-      </Exercise>
-      <Exercise
-        color="secondary"
-        onClick={() => {
-          console.log("second button");
-        }}
-      >
-        Click here too
-      </Exercise>
-      <ListGroup
-        items={items}
-        heading="Places"
-        onSelectItem={handleSelectItem}
-      />
+    <div>
+      <Header />
+      <div className="alert alert-primary">
+        <Alert>
+          Hello <span>World</span>
+        </Alert>
+        <Exercise
+          color="danger"
+          onClick={() => {
+            console.log("first button");
+          }}
+        >
+          Click me!
+        </Exercise>
+        <Exercise
+          color="secondary"
+          onClick={() => {
+            console.log("second button");
+          }}
+        >
+          Click here too
+        </Exercise>
+        <ListGroup
+          items={items}
+          heading="Places"
+          onSelectItem={handleSelectItem}
+        />
+      </div>
     </div>
   );
 }
